cedf6679cbd31525d6eeb0b3b2556c019ee6ec5f
Volodymyr Chernenko <vovan.trener@gmail.com>
2025-08-30 16:04:21 +0300

    tests SQL

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -17,7 +17,7 @@ def test_user_not_exists(github_api):
 def test_repo_can_be_found(github_api):
     r = github_api.search_repo('become-qa-auto')
     assert r.get('total_count') == 57
-    assert 'become-qa-auto' in r['items'] [0] ['name']
+    assert 'become-qa-auto' in r.get('items') [0] ['name']
 
 
 @pytest.mark.api
@@ -30,3 +30,24 @@ def test_repo_cannot_be_found(github_api):
 def test_repo_with_single_char_be_found(github_api):
     r = github_api.search_repo('s')
     assert r.get('total_count') != 0
+
+
+@pytest.mark.api
+def test_emojis(github_api):
+    r = github_api.get_emojis()
+
+    assert r['abc'] == 'https://github.githubassets.com/images/icons/emoji/unicode/1f524.png?v8'
+
+
+@pytest.mark.api
+def test_commits(github_api):
+    r = github_api.list_commits('Volodymyr27ux','TEST_QA')
+    
+    assert r[0]['commit']['author']['name'] == 'Volodymyr Chernenko'
+    assert r[1]['author']['login'] == 'Volodymyr27ux'
+
+@pytest.mark.api
+def test_not_found_commits(github_api):
+    r = github_api.list_commits('Volodymyr27','TEST_QA')
+
+    assert r['message'] == 'Not Found'
--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -23,3 +23,16 @@ def test_status_code_request():
     r = requests.get('https://api.github.com/users/sergii_butenko')
 
     assert r.status_code == 404
+
+@pytest.mark.http
+def test_status_code_emojis():
+    r = requests.get('https://api.github.com/emojis')
+
+    assert r.status_code == 200
+
+@pytest.mark.http
+def test_commits():
+    r = requests.get('https://api.github.com/repos/Volodymyr27ux/TEST_QA/commits')
+    
+    assert r.status_code == 200
+    
